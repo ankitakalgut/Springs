bf8e535bfb4fe65445baf47c9331d7eb46690815
ankitakalgut <kalgutkarankita@yahoo.in>
2018-07-03 17:30:16 +0530

    AutowiredProperties

---
+++ SpellChecker.java
@@ -0,0 +1,15 @@
+package com.bridgelabzSprings.AutowiredAnnotationProperties;
+
+public class SpellChecker 
+{
+   public SpellChecker()
+   {
+      System.out.println("Inside SpellChecker constructor." );
+   }
+
+   public void checkSpelling()
+   {
+      System.out.println("Inside checkSpelling." );
+   }  
+}
+
---
+++ TextEditor.java
@@ -0,0 +1,25 @@
+
+package com.bridgelabzSprings.AutowiredAnnotationProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor 
+{
+   @Autowired
+   private SpellChecker spellChecker;
+
+   public TextEditor() 
+   {
+      System.out.println("Inside TextEditor constructor." );
+   }
+   
+   public SpellChecker getSpellChecker( )
+   {
+      return spellChecker;
+   }
+   
+   public void spellCheck()
+   {
+      spellChecker.checkSpelling();
+   }
+}
